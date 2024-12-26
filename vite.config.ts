e53911c43a53ba5a731920a52371739d6ed5a001
lainlee <lainlixf@gmail.com>
2024-12-26 17:50:04 +0800

    feat: 增加 auto-import / vue-components 包及相关 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import postcssPlugins from './postcss.config.cjs'
 import { UnifiedViteWeappTailwindcssPlugin as uvwt } from 'weapp-tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 const isH5 = process.env.UNI_PLATFORM === 'h5'
 const isApp = process.env.UNI_PLATFORM === 'app'
@@ -14,7 +16,25 @@ export default defineConfig({
     uvwt({
       disabled: WeappTailwindcssDisabled,
       rem2rpx: true
-    }) as any
+    }) as any,
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: [
+        'vue',
+        '@vueuse/core',
+        'uni-app'
+      ],
+      ignore: ['useFetch'],
+    }),
+    Components({
+      directoryAsNamespace: true,
+      collapseSamePrefixes: true,
+    })
   ],
   css: {
     postcss: {
